c7b8af4151eef3a8add5a6f5da071aafba5f90c9
AJITHDEV C <60468028+Ajithdev444@users.noreply.github.com>
2022-09-26 14:41:26 +0530

    Add files via upload

---
+++ build/precache-manifest.d800cd0063592a3989f4366194133e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99fb8dffb61252dedb7",
+    "url": "/static/css/main.831e5a71.chunk.css"
+  },
+  {
+    "revision": "e99fb8dffb61252dedb7",
+    "url": "/static/js/main.e99fb8df.chunk.js"
+  },
+  {
+    "revision": "7ef873392d73c34b4b72",
+    "url": "/static/js/1.7ef87339.chunk.js"
+  },
+  {
+    "revision": "d37d01b767e34a92dadf",
+    "url": "/static/js/2.d37d01b7.chunk.js"
+  },
+  {
+    "revision": "9c1e9fa0fb3e25a65808",
+    "url": "/static/js/runtime~main.9c1e9fa0.js"
+  },
+  {
+    "revision": "ea8b4994a28a9064afbbb6108c522589",
+    "url": "/index.html"
+  }
+];
